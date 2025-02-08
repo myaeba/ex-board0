3603b91141e24ebe5710d201b8983fb5618baf72
myaeba <lim104321@naver.com>
2025-02-08 18:53:11 +0900

    #4 - 스프링 부트 프로젝트 시작

---
+++ src/main/java/com/example/exboard0/ExBoard0Application.java
@@ -0,0 +1,13 @@
+package com.example.exboard0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBoard0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExBoard0Application.class, args);
+    }
+
+}
